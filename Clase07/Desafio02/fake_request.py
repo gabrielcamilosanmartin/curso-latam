cad5507bcf69a8382bb2df1ed07c8f0403b79a65
Gabriel Camilo <gabriel.camilo@ideauno.cl>
2019-05-29 11:58:15 -0400

    fix desafio 2 clase 7

--- Clase07/Desafio02/fake_request.py
+++ Clase07/Desafio02/fake_request.py
@@ -1,9 +1,9 @@
 import requests
 
-def request(method, url, payload):
+def request(method, url, payload = {}):
 
 	if method == "GET":
-		response = requests.request(method, url, params=payload)
+		response = requests.request(method, url)
 	else:
 		response = requests.request(method, url, data=payload)
 
@@ -13,7 +13,7 @@ def request(method, url, payload):
 		return response.json()
 
 
-print(request("GET", "https://reqres.in/api/users", {}))
+print(request("GET", "https://reqres.in/api/users"))
 print(request("POST", "https://reqres.in/api/users", {"name": "prueba", "job": "prueba"}))
 print(request("PUT", "https://reqres.in/api/users/2", {"name": "prueba", "job": "prueba"}))
 print(request("DELETE", "https://reqres.in/api/users/2", {}))
